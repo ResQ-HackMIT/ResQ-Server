9f74eca2b8be8d0425a5cc10059278158257c2fa
Cooper Pellaton <c@cepp.ch>
2017-09-16 16:04:54 -0400

    Location syncing.

--- auth.ts
+++ auth.ts
@@ -1,11 +1,18 @@
 import * as crypto from "crypto";
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import { IUser, User, IFirstResponder, FirstResponder } from "./schema";
+import {
+    IUser,
+    User,
+    IFirstResponder,
+    FirstResponder,
+    ILocation,
+    Location
+} from "./schema";
 
 export const authRoutes = express.Router();
 
-const USER_DEFAULT: Partial<IUser> = {
+const USER_DEFAULT: Partial < IUser > = {
     "name": "Aaron Vontell",
     "medicalConditions": ["Obesity II", "Diabetes"],
     "allergies": ["Peanut Butter", "Cats"],
@@ -18,7 +25,13 @@ const USER_DEFAULT: Partial<IUser> = {
     "spouse": false
 };
 
-authRoutes.route("/user").post(bodyParser.json(), async (request, response) => {
+const LOCATION_DEFAULT: Partial < ILocation > = {
+    "userAuthKey": "00000000000000000000000000000000",
+    "lat": 42.359221,
+    "long": -71.093003
+}
+
+authRoutes.route("/user").post(bodyParser.json(), async(request, response) => {
     const key = crypto.randomBytes(32).toString("hex");
 
     await new User({
@@ -42,14 +55,28 @@ authRoutes.route("/user").post(bodyParser.json(), async (request, response) => {
     });
 });
 
-const FIRST_RESPONDER_DEFAULT: Partial<IFirstResponder> = {
+authRoutes.route("/location").post(bodyParser.json(), async(request, response) => {
+    const key = crypto.randomBytes(32).toString("hex");
+    await new Location({
+        "userAuthKey": request.body.userAuthKey || LOCATION_DEFAULT.userAuthKey,
+        "lat": request.body.lat || LOCATION_DEFAULT.lat,
+        "long": request.body.long || LOCATION_DEFAULT.long
+    }).save();
+
+    response.status(201).json({
+        "success": true,
+        "authorizationKey": key
+    });
+});
+
+const FIRST_RESPONDER_DEFAULT: Partial < IFirstResponder > = {
     "name": "Cooper Pellaton",
     "hasBoat": false,
     "hasCar": false,
     "physicality": 0
 };
 
-authRoutes.route("/firstresponder").post(bodyParser.json(), async (request, response) => {
+authRoutes.route("/firstresponder").post(bodyParser.json(), async(request, response) => {
     const key = crypto.randomBytes(32).toString("hex");
 
     await new FirstResponder({
--- schema.ts
+++ schema.ts
@@ -2,11 +2,12 @@ import * as mongoose from "mongoose";
 
 (mongoose as any).Promise = global.Promise;
 mongoose.connect("mongodb://localhost/resq", {
-	useMongoClient: true
-} as mongoose.ConnectionOptions);
+        useMongoClient: true
+    }
+    as mongoose.ConnectionOptions);
 
 export interface IUser {
-	_id: mongoose.Types.ObjectId;
+    _id: mongoose.Types.ObjectId;
     name: string;
     medicalConditions: string[];
     allergies: string[];
@@ -21,9 +22,21 @@ export interface IUser {
     authorizationKey: string;
 }
 
-export const User = mongoose.model<IUser & mongoose.Document>("User", new mongoose.Schema({
-	name: String,
-	medicalConditions: [String],
+export interface ILocation {
+    userAuthKey: string;
+    lat: number;
+    long: number;
+}
+
+export const Location = mongoose.model < ILocation & mongoose.Document > ("Location", new mongoose.Schema({
+    userAuthKey: String,
+    lat: Number,
+    long: Number
+}));
+
+export const User = mongoose.model < IUser & mongoose.Document > ("User", new mongoose.Schema({
+    name: String,
+    medicalConditions: [String],
     allergies: [String],
     medications: [String],
     weight: Number,
@@ -43,7 +56,7 @@ export interface IFirstResponder {
     physicality: number;
 }
 
-export const FirstResponder = mongoose.model<IFirstResponder & mongoose.Document>("FirstResponder", new mongoose.Schema({
+export const FirstResponder = mongoose.model < IFirstResponder & mongoose.Document > ("FirstResponder", new mongoose.Schema({
     name: String,
     hasBoat: Boolean,
     hasCar: Boolean,
