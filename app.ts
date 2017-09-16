e960c967fce4544ff6f16c463d5011895878df77
Cooper Pellaton <c@cepp.ch>
2017-09-16 14:52:25 -0400

    Sample JSON for post.

--- app.ts
+++ app.ts
@@ -10,6 +10,31 @@ process.on("unhandledRejection", err => {
 	throw err;
 });
 
+
+// Expected post sample:
+// {
+// 		"name": "Aaron Vontell",
+// 		"medicalConditions": "Obesity II, Diabetes",
+//     	"allergies": "Peanut Butter, Cats",
+//     	"medications": "Laxatives, Penicillin",
+// 		"weight": "260",
+//     	"height": "5'11\"",
+// 		"age": "20",
+//     	"kids": "0",
+//     	"animals": "0",
+//     	"spouse": "false"
+// }
+// 
+// Expected return:
+//  {
+// 	 	"success": "true"
+//  }
+app.route("/api/create-account").get((request, response) => {
+	response.json({
+		"success": "true"
+	})
+});
+
 app.route("/version").get((request, response) => {
 	response.json({
 		"version": "0.0.1",
