74a0b985bd9c6156d2b48a1d11ddbc80b35cab02
Ryan Petschek <petschekr@gmail.com>
2017-09-16 22:27:39 -0400

    Add location when importing

--- importer.ts
+++ importer.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import * as geolib from "geolib";
 import { IUser, User } from "./schema";
 
 const csv = require("csv-parser");
@@ -6,10 +7,16 @@ const csv = require("csv-parser");
 (async () => {
     await User.collection.drop();
 
+    // At Houston's airport
+    const center: geolib.PositionAsDecimal = { latitude: 29.9494083, longitude: -95.4385523 };
+
     let userDocs: Partial<IUser>[] = [];
     fs.createReadStream("data.csv", {encoding: "utf8"})
         .pipe(csv())
         .on("data", async (data: any) => {
+            let locationProximity: number = parseInt(data.location, 10);
+            let location = geolib.computeDestinationPoint(center, Math.random() * locationProximity * 1000 + 500 * locationProximity, Math.random() * 360);
+
             userDocs.push({
                 "name": data.name,
                 "medicalConditions": parseInt(data.medicalConditions, 10),
@@ -22,10 +29,14 @@ const csv = require("csv-parser");
                 "animals": parseInt(data.animals, 10),
                 "spouse": data.spouse === "1",
                 "hasTransportation": data.hasTransportation === "1",
-                "locationProximity": parseInt(data.location, 10)
+                "locationProximity": parseInt(data.location, 10),
+                "location": [{
+                    "lat": location.latitude,
+                    "long": location.longitude
+                }]
             });
         }).on("end", async () => {
-            User.insertMany(userDocs);
+            await User.insertMany(userDocs);
             console.log(`Imported ${userDocs.length} documents`);
             process.exit(0);
         });
